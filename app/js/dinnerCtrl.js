a5408d59c8f861e9190b8d6aac30781e79232eae
Trulls <annaj9@kth.se>
2015-04-28 00:05:15 +0200

    Deleted unnecessary comments

--- app/js/dinnerCtrl.js
+++ app/js/dinnerCtrl.js
@@ -1,23 +1,22 @@
 // Dinner controller that we use whenever we have view that needs to 
 // display or modify the dinner menu
+
 dinnerPlannerApp.controller('DinnerCtrl', function ($scope,Dinner) {
 	// TODO in Lab 5: Implement the methods to get the dinner menu
-  // add dish to menu and get total menu price
+	// add dish to menu and get total menu price
 
 	$scope.numberOfGuests = Dinner.getNumberOfGuests();
 
-		$scope.setNumberOfGuest = function(number){
-			Dinner.setNumberOfGuests(number);
-		}
+	$scope.setNumberOfGuest = function(number){
+		Dinner.setNumberOfGuests(number);
+	}
 
-		$scope.getNumberOfGuests = function() {
-			return Dinner.getNumberOfGuests();
-		}
+	$scope.getNumberOfGuests = function() {
+		return Dinner.getNumberOfGuests();
+	}
 
-
-
-  // Dinner.DishSearch.get({title_kw:'chicken'}) 
-  // or to get a single dish we would do 
-  // Dinner.Dish.get({id:12345}).
+	// Dinner.DishSearch.get({title_kw:'chicken'}) 
+	// or to get a single dish we would do 
+	// Dinner.Dish.get({id:12345}).
 
 });
