d506d48bec8cb99fd6344f3ca8a7f582836e29b9
Trulls <annaj9@kth.se>
2015-05-03 19:24:45 +0200

    Added templates for the last two views

--- app/js/app.js
+++ app/js/app.js
@@ -48,6 +48,14 @@ dinnerPlannerApp.config(['$routeProvider',
         templateUrl: 'partials/dish.html',
         controller: 'DishCtrl'
       }).
+      when('/overview', {
+        templateUrl: 'partials/overview.html',
+        controller: 'DinnerCtrl'
+      }).
+      when('/preparation', {
+        templateUrl: 'partials/preparation.html',
+        controller: 'DinnerCtrl'
+      }).
       // TODO in Lab 5: add more conditions for the last two screens (overview and preparation)
       otherwise({
         redirectTo: '/home'
