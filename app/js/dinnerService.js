18c3d251e22af20801c0e5f55770280e2b94e899
Trulls <annaj9@kth.se>
2015-05-04 00:19:28 +0200

    Angular cookie now saves the dinner (menu and number of guests) in a cookie and reloads it at launch

--- app/js/dinnerService.js
+++ app/js/dinnerService.js
@@ -4,87 +4,110 @@
 // service is created first time it is needed and then just reuse it
 // the next time.
 dinnerPlannerApp.factory('Dinner',function ($resource,$cookieStore) {
-  // TODO in Lab 5: Add your model code from previous labs
-  // feel free to remove above example code
-  // you will need to modify the model (getDish and getAllDishes) 
-  // a bit to take the advantage of Angular resource service
-  // check lab 5 instructions for details
-  
-  var numberOfGuests = 4;
-  var menu = {};
+	// TODO in Lab 5: Add your model code from previous labs
+	// feel free to remove above example code
+	// you will need to modify the model (getDish and getAllDishes) 
+	// a bit to take the advantage of Angular resource service
+	// check lab 5 instructions for details
 
-  //Sets the number of guests
-  this.setNumberOfGuests = function(num) {
-    numberOfGuests = num;
-  }
+	this.DishSearch = $resource(
+		'http://api.bigoven.com/recipes',
+		{pg:1,rpp:25,api_key:'dvx6H6QTYoSVG1J9p9BaIcf097ZInDlP'}
+	);
 
-  //Returns the number of guests
-  this.getNumberOfGuests = function() {
-    return numberOfGuests;
-  }
+	this.Dish = $resource(
+		'http://api.bigoven.com/recipe/:id',
+		{api_key:'dvx6H6QTYoSVG1J9p9BaIcf097ZInDlP'}
+	);
 
-  //Returns the full menu with all the dishes.
-  this.getFullMenu = function () {
-    return menu;
-  };
+	var numberOfGuests = 4;
 
-  //Returns all ingredients for all the dishes on the menu.
-  this.getAllIngredients = function () {
-    ingredients = [];
-    menu.forEach(function (dish) {
-      ingredients.push(_.pluck(dish.ingredients, 'name'));
-    });
-    return _.uniq(_.flatten(ingredients));
-  };
+	if($cookieStore.get('numberOfGuests')!=undefined) {
+		numberOfGuests = $cookieStore.get('numberOfGuests'); 
+	}
+	
+	var confirmedMenu = [];
+	var menu = {};
 
-  //Returns the price of a specific dish (all the 
-  //ingredients multiplied by number of guests).
-  this.getPriceOfDish = function (dish) {
-    var unitCost = 0;
-    _.each(dish.Ingredients, function (ingredient) {
-      unitCost += ingredient.Quantity;
-    }, this);
-    return unitCost * numberOfGuests;
-  };
+	if($cookieStore.get('confirmedMenu')!=undefined) {
+		confirmedMenu = $cookieStore.get('confirmedMenu'); 
+		_.each(confirmedMenu, function (RecipeID) {
+			var dish = this.Dish.get({id:RecipeID}, function () {
+				menu[dish.Category] = dish;
+			}, this);
+		}, this);
+	}
 
-  //Returns the total price of the menu (all the 
-  //ingredients multiplied by number of guests).
-  this.getTotalMenuPrice = function () {
-    var cost = 0;
-    _.each(menu, function (dish) {
-      cost += this.getPriceOfDish(dish);
-    }, this);
-    return cost;
-  };
+	//Sets the number of guests
+	this.setNumberOfGuests = function(num) {
+		numberOfGuests = num;
+		$cookieStore.put('numberOfGuests',num);
+	}
 
-  //Adds the passed dish to the menu. If the dish of that 
-  //category already exists on the menu it is removed from 
-  //the menu and the new one added.
-  this.addDishToMenu = function (dish) {
-    menu[dish.Category] = dish;
-  }
+	//Returns the number of guests
+	this.getNumberOfGuests = function() {
+		return numberOfGuests;
+	}
 
-  //Removes dish from menu
-  this.removeDishFromMenu = function (id) {
-    _.reject(menu, function (dish) {
-      return dish.id === id;
-    });
-  }
+	//Returns the full menu with all the dishes.
+	this.getFullMenu = function () {
+		confirmedMenu = [];
+		_.each(menu, function (dish) {
+			confirmedMenu.push(dish.RecipeID);
+		}, this);
+		$cookieStore.put('confirmedMenu',confirmedMenu);
+		return menu;
+	};
 
-  this.DishSearch = $resource(
-    'http://api.bigoven.com/recipes',
-    {pg:1,rpp:25,api_key:'dvx6H6QTYoSVG1J9p9BaIcf097ZInDlP'}
-  );
+	//Returns all ingredients for all the dishes on the menu.
+	this.getAllIngredients = function () {
+		ingredients = [];
+		_.each(menu, function (dish) {
+			ingredients.push(_.pluck(dish.ingredients, 'name'));
+		});
+		return _.uniq(_.flatten(ingredients));
+	};
 
-  this.Dish = $resource(
-    'http://api.bigoven.com/recipe/:id',
-    {api_key:'dvx6H6QTYoSVG1J9p9BaIcf097ZInDlP'}
-  );
+	//Returns the price of a specific dish (all the 
+	//ingredients multiplied by number of guests).
+	this.getPriceOfDish = function (dish) {
+		var unitCost = 0;
+		_.each(dish.Ingredients, function (ingredient) {
+			unitCost += ingredient.Quantity;
+		}, this);
+		return unitCost * numberOfGuests;
+	};
 
-  // Angular service needs to return an object that has all the
-  // methods created in it. You can consider that this is instead
-  // of calling var model = new DinnerModel() we did in the previous labs
-  // This is because Angular takes care of creating it when needed.
-  return this;
+	//Returns the total price of the menu (all the 
+	//ingredients multiplied by number of guests).
+	this.getTotalMenuPrice = function () {
+		var cost = 0;
+		_.each(menu, function (dish) {
+			cost += this.getPriceOfDish(dish);
+		}, this);
+		return cost;
+	};
+
+	//Adds the passed dish to the menu. If the dish of that 
+	//category already exists on the menu it is removed from 
+	//the menu and the new one added.
+	this.addDishToMenu = function (dish) {
+		menu[dish.Category] = dish;
+	}
+
+	//Removes dish from menu
+	this.removeDishFromMenu = function (id) {
+		_.reject(menu, function (dish) {
+			return dish.id === id;
+		});
+	}
+
+
+
+	// Angular service needs to return an object that has all the
+	// methods created in it. You can consider that this is instead
+	// of calling var model = new DinnerModel() we did in the previous labs
+	// This is because Angular takes care of creating it when needed.
+	return this;
 
 });
